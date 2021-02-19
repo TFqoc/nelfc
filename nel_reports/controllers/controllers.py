6de9bf8442c0b0dd0a1ec4d7b52bdf257eb388b6
TFqoc <tfullmer@qocinnovations.com>
2021-02-19 19:19:47 +0000

    Scaffold

---
+++ nel_reports/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class ../user/nelReports(http.Controller):
+#     @http.route('/../user/nel_reports/../user/nel_reports/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/../user/nel_reports/../user/nel_reports/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('../user/nel_reports.listing', {
+#             'root': '/../user/nel_reports/../user/nel_reports',
+#             'objects': http.request.env['../user/nel_reports.../user/nel_reports'].search([]),
+#         })
+
+#     @http.route('/../user/nel_reports/../user/nel_reports/objects/<model("../user/nel_reports.../user/nel_reports"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('../user/nel_reports.object', {
+#             'object': obj
+#         })
